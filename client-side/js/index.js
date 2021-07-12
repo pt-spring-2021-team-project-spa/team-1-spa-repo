b4966a78e982f7677375b62caea7874902fdc0ed
BrentHajek <brenthajek@hotmail.com>
2021-07-11 22:53:52 -0400

    basic buttons and nav created and linked to pages

--- client-side/js/index.js
+++ client-side/js/index.js
@@ -1,1 +1,49 @@
-console.log('Client Side is wired up!');
+import AboutUsPage from './pages/AboutUsPage.js';
+import Footer from './components/Footer.js';
+import GamesPage from './pages/GamesPage.js';
+import Header from './components/Header.js';
+import HomePage from './pages/HomePage.js';
+
+buildPage();
+
+function buildPage() {
+    footer();
+    header();
+    navigateToAboutUsPage();
+    navigateToGamesPage();
+    navigateToHomePage();
+}
+
+function footer() {
+    const footerElement = document.querySelector('.footer');
+    footerElement.innerHTML = Footer();
+}
+
+function header() {
+    const headerElement = document.querySelector('.header');
+    headerElement.innerHTML = Header();
+}
+
+function navigateToAboutUsPage() {
+    const homeButton = document.querySelector('.nav__list_about_us');
+    homeButton.addEventListener('click', () => {
+      const app = document.querySelector('#appMain');
+      app.innerHTML = AboutUsPage();
+    });
+}
+
+function navigateToGamesPage() {
+    const homeButton = document.querySelector('.nav__list_games');
+    homeButton.addEventListener('click', () => {
+      const app = document.querySelector('#appMain');
+      app.innerHTML = GamesPage();
+    });
+}
+
+function navigateToHomePage() {
+    const homeButton = document.querySelector('.nav__list_home');
+    homeButton.addEventListener('click', () => {
+      const app = document.querySelector('#appMain');
+      app.innerHTML = HomePage();
+    });
+}
---
+++ client-side/js/pages/AboutUsPage.js
@@ -0,0 +1,7 @@
+export default function AboutUsPage() {
+    return `
+    <h2>Welcome to the About Us Page</h2>
+    <h3>Meet our team</h3>
+    <p>information on team members to be added later</p>
+    `;
+}
