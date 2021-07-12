3898aa65e6cabf8f01daf693b16008387144cb18
tonyblankenship96 <tony.blankenship96@gmail.com>
2021-07-12 13:28:15 -0400

    models are done

---
+++ src/main/java/org/wecancodeit/mysteryeducator/models/Animal.java
@@ -0,0 +1,73 @@
+package org.wecancodeit.mysteryeducator.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.util.Objects;
+
+@Entity
+public class Animal {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String animalName;
+    private String description;
+    private String avgLifeSpan;
+
+
+    @ManyToOne
+    private Continent continent;
+
+    @ManyToOne
+    private Classification classification;
+
+    public Animal(){}
+
+    public Animal(String animalName, String description, String avgLifeSpan, Continent continent, Classification classification) {
+        this.animalName = animalName;
+        this.description = description;
+        this.avgLifeSpan = avgLifeSpan;
+        this.continent = continent;
+        this.classification = classification;
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public String getAnimalName(){
+        return animalName;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public String getAvgLifeSpan(){
+        return avgLifeSpan;
+    }
+
+    public Continent getContinent(){
+        return continent;
+    }
+
+    public Classification getClassification(){
+        return classification;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Animal animal = (Animal) o;
+        return Objects.equals(id, animal.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
--- src/main/java/org/wecancodeit/mysteryeducator/repos/MysteryEducatorApplication.java
+++ src/main/java/org/wecancodeit/mysteryeducator/repos/MysteryEducatorApplication.java
@@ -1,4 +1,4 @@
-package org.wecancodeit.mysteryeducator;
+package org.wecancodeit.mysteryeducator.repos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
