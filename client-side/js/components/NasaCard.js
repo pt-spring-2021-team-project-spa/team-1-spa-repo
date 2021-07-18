49b33e017338518c63f21193570ed3a50eaf78c1
YMcElroyWCCI <yuli.mcelroy@gmail.com>
2021-07-18 15:28:26 -0400

    adding date created into the Nasa Astronaut in Apollo mission page

--- client-side/js/components/NasaCard.js
+++ client-side/js/components/NasaCard.js
@@ -3,7 +3,8 @@ export default function NasaCard(nasaCard) {
     <div class='nasa__card_container'>
         <input type='hidden' id='nasaCardId' value=${nasaCard.data[0].nasa_id} />
         <h2 class='nasa__card_title'>${nasaCard.data[0].title}</h2>
-        <h3>${nasaCard.data[0].nasa_id}</h3>
+        <h3>NASA ID: ${nasaCard.data[0].nasa_id}</h3>
+        <h3>Date created: ${nasaCard.data[0].date_created}</h3>
         <div class='Nasa__card_img'><img src=${nasaCard.links[0].href} alt='' /></div>
         <p class='Nasa__card_desc'>Description: ${nasaCard.data[0].description}</p>
     </div>
