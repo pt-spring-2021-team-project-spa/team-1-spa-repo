81c98ec462e91cf9e233d3909dc318abd547d2e7
tonyblankenship96 <tony.blankenship96@gmail.com>
2021-07-23 20:45:38 -0400

    probably final

--- client-side/js/components/Header.js
+++ client-side/js/components/Header.js
@@ -5,12 +5,9 @@ export default function Header() {
           <button class='nav__list_about_us'>About Us</button>
           <button class='nav__list_meet_our_team'>Meet Our Team</button>
           <button class='nav__list_our_offices'>Our Offices</button>
-          <button class='nav__list_metMuseum'>Met Museum</button>
-          <button class='nav__list_reviews'>Reviews</button>
-          <button class='nav__list_donate'>Donate</button>
+          <button class='nav__list_metMuseum'>Met museum</button>
           <button class='nav__list_we_are_hiring'>We're Hiring</button>
           <button class='nav__list_animals'>Animals</button>
-          <button class='nav__list_nasaCard'>NASA</button>
           <button class='nav__list_nasaCards'>NASA List</button>
       </li>
       `;
